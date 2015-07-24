79b6dd02bdccacd3404a62ac83aab03f75350ae1
Tukei <tukeitoday@gmail.com>
2015-07-24 15:28:16 +0300

    added database

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,11 @@
 class Page < ActiveRecord::Base
+	validates :name, presence:  true
+	validates :category, presence:  true
+	validates :section, presence:  true
+	validates :title, presence:  true
+	validates :description, presence:  true
+	
+ 	def category_enum
+	['software', 'graphics', 'networking']
+	end
 end
